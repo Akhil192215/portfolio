275fd54086d1eeef3c1abee85c8cc8d4191b13d0
akhilspoovathinkal <akhilspoovathinkal@gmail.com>
2023-08-02 12:45:34 +0530

    about content-update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,39 @@
-
-
 const About = () => {
   return (
-    <div data-name="about" className='w-full  h-screen bg-gradient-to-b from-black via-[black] to-yellow-950'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className=' text-[#f2ab45] text-4xl font-bold border-b-4 inline border-secondary    '>
-                    About
-                </p>
-            </div>
-            <div  className='text-secondary text-xl'>
-                <p className='mt-8'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Optio, illo voluptatum facere est cum sit vitae veritatis 
-                    nemo sunt animi nam facilis dignissimos libero accusamus 
-                    eveniet deleniti! Quas vitae, quos non blanditiis perspiciatis 
-                    officia molestias unde, ipsa excepturi provident autem qui soluta 
-                    veritatis consectetur repudiandae ab. Numquam amet dolorum et.
-                </p>
-                <br />
-
-                <p >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Odio praesentium aut, soluta, minus amet quibusdam eligendi 
-                    recusandae nam aliquam eos eius dolorem? Suscipit porro molestiae
-                     nisi officiis sed debitis error voluptates eaque tempora delectus 
-                     magni voluptatibus quidem deleniti aliquam, sint ipsum
-                     et repudiandae odio, soluta, sapiente eius consequatur blanditiis. Dicta.
-                </p>
-            </div>
+    <div
+      data-name="about"
+      className="w-full  h-screen bg-gradient-to-b from-black via-[black] to-yellow-950"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className=" text-[#f2ab45] text-4xl font-bold border-b-4 inline border-secondary    ">
+            About
+          </p>
         </div>
-    </div>
-  )
-}
+        <div className="text-secondary text-xl">
+          <p className="mt-8">
+            I'm an experienced MERN stack web developer with a robust skill set
+            and a passion for solving problems. With expertise in MongoDB, Node
+            js, Express js, React., Postman, AWS (EC2, R-53), Git and GitHub,
+            Rest API, JWT, Chakra UI, HTML, and CSS, I have developed
+            interactive user interfaces with responsive designs and custom admin
+            panels.
+          </p>
+          <br />
 
-export default About
+          <p>
+            I have worked on various projects, including Coders House, a code
+            collaboration app for programmers, and CakeShop, an e-commerce site
+            for selling cakes. My projects showcase proficiency in API
+            integrations, user authorization and authentication, and deployment
+            on AWS. I additionally, contributed to projects like Netflix Trailer
+            App, which involved API integration and displaying movies
+            dynamically.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
